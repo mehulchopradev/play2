5dbfed8298de4cd335b8dcbf3ab45acb593bcf0e
mehul chopra <mehul.chopra.dev@gmail.com>
2017-07-28 22:32:42 +0530

    date api initial commit

---
+++ date.py
@@ -0,0 +1,1 @@
+# a function that returns the current date and time in dd/MM/yyyy format
--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+# import the module date.py
+
 print "Hello To All"
 
 print "Good Morning To All"
 
 print "Good Evening"
+
+# call the function from the above imported module and print it!
