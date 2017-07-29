4f7728b7b0c30e6a8221ad2d735baf85393e60a4
mehul chopra <mehul.chopra.dev@gmail.com>
2017-07-29 10:01:29 +0530

    student class feature

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 # import the module date.py
+# import the module student.py
 
 print "Hello To All"
 
@@ -9,3 +10,6 @@ msg = 'Good Night'
 print msg
 
 # call the function from the above imported module and print it!
+
+
+#create a student object and call the various instance methods on it to verify the result
---
+++ student.py
@@ -0,0 +1,1 @@
+# student class that will have instance methods, constructor and class attributes
